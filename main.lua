95e3b431640d9c88eaabb431f11e14d8c621c4e9
CriticalFloof <zackbig2005@gmail.com>
2026-03-25 18:08:43 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,19 +1,30 @@
 --! file: main.lua
 
 local _ = require( "units.debug" )
+local Vector2 = require( "units.vector2" )
+local TileGrid = require( "units.tile_grid" )
+local Ore = require( "units.ore" )
+local Upgrader = require( "units.upgrader" )
+local Conveyor = require( "units.conveyor" )
+local UpgraderBeam = require( "units.upgrader_beam" )
 
 local UPGRADER_PIXEL_OFFSET = 2 -- The texture is offset by 2 pixels.
 
 local COLOR_WHITE = { 1, 1, 1, 1 }
-local COLOR_ORE = { 0, 0.5, 1, 1 }
+local COLOR_ORE_ORANGE = { 0.5, 0.75, 0, 1 }
+local COLOR_ORE = { 1, 0.75, 0, 1 }
 local COLOR_BEAM = { 0, 1, 1, 0.3 }
 
 local ores = {}
 
-local upgrader = {}
-local beam = {}
+local upgrader
+local beam
 
 function love.load()
+
+    love.graphics.setDefaultFilter( "nearest", "nearest" )
+
+    --[[
     upgrader.x = 200
     upgrader.y = 200
     upgrader.size = 4
@@ -21,27 +32,34 @@ function love.load()
     upgrader.width = upgrader.image:getWidth() * upgrader.size
     upgrader.height = upgrader.image:getHeight() * upgrader.size
     upgrader.beam = beam
+    ]]
 
-    upgrader.image:setFilter( "nearest", "nearest" )
-
-    beam.x = upgrader.x + ( upgrader.width / 2 ) - upgrader.size * 2
-    beam.y = upgrader.y + upgrader.size * UPGRADER_PIXEL_OFFSET
-    beam.width = 4 -- why?
-    beam.height = upgrader.height - upgrader.size * 4
-    beam.color = COLOR_BEAM
+    local upgraderPos = Vector2( 200, 200 )
+    local upgraderSize = Vector2( 4, 4 )
+    local upgraderTag = { "upgrader_tier1", 1 } -- Ore: { upgrader_tier1 = { current, max } }
+    
+    upgrader = Upgrader( upgraderPos, upgraderSize, upgraderTag )
+    
+    beam = UpgraderBeam(
+        Vector2( upgrader.x + ( upgrader.width / 2 ) - upgrader.size * 2, upgrader.y + upgrader.size * UPGRADER_PIXEL_OFFSET ),
+        Vector2( upgrader.size * 4, upgrader.height - upgrader.size * 4 ),
+        function( ore )
+            return ore.value * 2
+        end
+    )
 
     print( beam.width )
 end
 
 local function makeOre()
-    local ore = {}
-    ore.x = 100
-    ore.y = 190 + upgrader.height / 2
-    ore.width = 20
-    ore.height = 20
-    ore.speed = 50
-    ore.color = COLOR_ORE
-
+    local pos = Vector2( 100, 190 + upgrader.height / 2 )
+    local size = Vector2( 20, 20 )
+    local speed = 50
+    local vel = Vector2( speed, 0 )
+    local color = COLOR_ORE_ORANGE
+    
+    local value = 5
+    local ore = Ore( pos, size, vel, color, value )
     table.insert( ores, ore )
 end
 
@@ -52,16 +70,17 @@ function love.keypressed( key )
 end
 
 local function checkCollisions( a, b )
-    local aLeft = a.x
-    local aRight = a.x + a.width
-    local aTop = a.y
-    local aBottom = a.y + a.height
 
-    local bLeft = b.x
-    local bRight = b.x + b.width
-    local bTop = b.y
-    local bBottom = b.y + b.height
+    local aLeft = a.pos.x
+    local aRight = a.pos.x + a.size.x
+    local aTop = a.pos.y
+    local aBottom = a.pos.y + a.size.y
 
+    local bLeft = b.pos.x
+    local bRight = b.pos.x + b.size.x
+    local bTop = b.pos.y
+    local bBottom = b.pos.y + b.size.y
+    
     return aRight > bLeft
         and aLeft < bRight
         and aBottom > bTop
@@ -70,23 +89,29 @@ end
 
 function love.update( dt )
     for _, ore in ipairs( ores ) do
-        ore.x = ore.x + ore.speed * dt
+        ore:update( dt )
 
         if checkCollisions( ore, beam ) then
-            ore.color = COLOR_BEAM
+            --ore.color = COLOR_BEAM
+            
+            local alpha = 0.01
+            ore.color = {
+                ( 1 - alpha ) * ore.color[1] + alpha * COLOR_BEAM[1],
+                ( 1 - alpha ) * ore.color[2] + alpha * COLOR_BEAM[2],
+                ( 1 - alpha ) * ore.color[3] + alpha * COLOR_BEAM[3],
+                ( 1 - alpha ) * ore.color[4] + alpha * COLOR_BEAM[4]
+            }
         end
     end
 end
 
 function love.draw()
+
     love.graphics.draw( upgrader.image, upgrader.x, upgrader.y, nil, upgrader.size, upgrader.size )
 
     for _, ore in ipairs( ores ) do
-        love.graphics.setColor( ore.color )
-        love.graphics.rectangle( "fill", ore.x, ore.y, ore.width, ore.height )
+        ore:draw()
     end
 
-    love.graphics.setColor( beam.color )
-    love.graphics.rectangle( "fill", beam.x, beam.y, beam.width * upgrader.size, beam.height )
-    love.graphics.setColor( COLOR_WHITE )
+    beam:draw()
 end
