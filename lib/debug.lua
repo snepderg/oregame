205d2690e9711f96f60f9e2eae845cc7d79081cc
CriticalFloof <zackbig2005@gmail.com>
2026-03-25 13:32:51 -0400

    Fix love errorhandler

--- lib/debug.lua
+++ lib/debug.lua
@@ -6,13 +6,13 @@ if arg[2] == "debug" then
     require( "lldebugger" ).start()
 end
 
-local love_errorhandler = love.errorhandler
+local default_errorhandler = love.errorhandler or love.errhand
 
 function love.errorhandler( msg )
     if lldebugger then
         error( msg, 2 )
     else
-        return love_errorhandler( msg )
+        return default_errorhandler( msg )
     end
 end
 
