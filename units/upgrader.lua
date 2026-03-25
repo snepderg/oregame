9f0093dd67b3ec26a725ae2a1e9d16678f6b54d8
CriticalFloof <zackbig2005@gmail.com>
2026-03-25 18:09:18 -0400

    Update upgrader Require

--- units/upgrader.lua
+++ units/upgrader.lua
@@ -1,10 +1,21 @@
+local Object = require( "external.classic" )
 local Building = require( "units.building" )
 
 local Upgrader = Building:extend()
+local UpgraderBeam = Object:extend()
 
+--[[
 -- Takes a callback, expects a number value to be returned
 function Upgrader:new( pos, size, tag )
     Upgrader.super.new( self, pos, size, "res/upgrader.png" )
+    self.tag = tag
+end
+]]
+
+function Upgrader:new( pos, size, tag, beamColor, callback )
+    self.pos = pos
+    self.size = size
+    self.beam = UpgraderBeam( pos, size, beamColor, callback )
     self.tag = tag
 end
 
