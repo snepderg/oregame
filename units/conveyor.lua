b5c687982812147a7ca8b6a4eb526ffc3ba4c23d
banoargeek <thatguymajor@protonmail.com>
2026-03-28 16:04:58 -0600

    Remove unused dependencies
    Some classes were referencing the `Vector2` class without using them - the instantiation handles this.

--- units/conveyor.lua
+++ units/conveyor.lua
@@ -1,13 +1,11 @@
 local Building = require( "units.building" )
-local Vector2 = require( "units.vector2" )
-
 local Conveyor = Building:subclass( "Conveyor" )
 
 
 ----- STATIC METHODS -----
 
 function Conveyor:initialize( pos, size )
-    Conveyor.super.initialize(self, pos, size, "res/belt.png")
+    Conveyor.super.initialize( self, pos, size, "res/belt.png" )
 end
 
 
