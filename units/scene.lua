1c3e43eac1f8601452614af119006cf53d3abd0e
CriticalFloof <zackbig2005@gmail.com>
2026-03-30 10:23:38 -0400

    Relative Transform for Scene Objects

--- units/scene.lua
+++ units/scene.lua
@@ -1,5 +1,7 @@
 local Destroyable = require( "classes.destroyable" )
 local GameObject = require( "units.game_object" )
+local GameObject2D = require( "units.game_object_2d" )
+local Camera = require( "units.camera" )
 
 local Scene = Destroyable:subclass( "Scene" )
 
@@ -10,12 +12,21 @@ function Scene:initialize()
     self._root = GameObject()
     self._root:_setScene( self )
     self._started = false
+    self._activeCamera = nil
 end
 
 ----- INSTANCE METHODS -----
 
 function Scene:start()
     self._started = true
+
+    -- Scenes require a camera to render objects, so if an active camera isn't present in the scene, we create one.
+    if self._activeCamera == nil then
+        local camera = Camera()
+        self._root:addChild( camera )
+        camera:makeCurrent()
+    end
+
     local function tickObjects( gameObject )
         for _, child in ipairs( gameObject:getChildren() ) do
             tickObjects( child )
@@ -38,6 +49,10 @@ function Scene:getRoot()
     return self._root
 end
 
+function Scene:getCurrentCamera()
+    return self._activeCamera
+end
+
 function Scene:tickUpdate( deltaTime )
     local function tickObjects( gameObject )
         gameObject:tickUpdate( deltaTime )
@@ -51,15 +66,41 @@ function Scene:tickUpdate( deltaTime )
 end
 
 function Scene:frameUpdate( deltaTime )
-    local function tickObjects( gameObject )
+    -- Setup
+    if self._activeCamera == nil then return end
+
+    local screenProjection = love.math.newTransform()
+    screenProjection:scale( 1, -1 )
+    screenProjection:translate( love.graphics.getWidth() / 2, -love.graphics.getHeight() / 2 )
+    -- Screen Space
+
+    -- View Space
+    love.graphics.push()
+    love.graphics.applyTransform( screenProjection )
+
+    -- World Space
+    love.graphics.push()
+    love.graphics.applyTransform( self._activeCamera:getViewProjection() )
+
+    local function drawObjects( gameObject )
         gameObject:frameUpdate( deltaTime )
+
+        -- Local Space
+        love.graphics.push()
+        if gameObject:isInstanceOf( GameObject2D ) then
+            love.graphics.applyTransform( gameObject:getModelProjection() )
+        end
+
         local children = gameObject:getChildren()
         for _, child in ipairs( children ) do
-            tickObjects( child )
+            drawObjects( child )
         end
+        love.graphics.pop()
     end
+    drawObjects( self._root )
 
-    tickObjects( self._root )
+    love.graphics.pop()
+    love.graphics.pop()
 end
 
 ----- IMPLEMENTED METHODS -----
