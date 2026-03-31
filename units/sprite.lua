d29d7b79f9d290cb2cd31113d8da9a1d16865e22
CriticalFloof <zackbig2005@gmail.com>
2026-03-30 22:49:57 -0400

    Create Sprite Class

---
+++ units/sprite.lua
@@ -0,0 +1,51 @@
+local GameObject2D = require( "units.game_object_2d" )
+local Vector2 = require( "units.vector2" )
+
+local Sprite = GameObject2D:subclass( "Sprite" )
+
+----- STATIC METHODS -----
+
+function Sprite:initialize( imagePath )
+    Sprite.super.initialize( self )
+
+    self.texture = love.graphics.newImage( imagePath )
+
+    self.flipH = false
+    self.flipV = false
+
+    self.centered = true
+    self.size = Vector2( self.texture:getWidth(), self.texture:getHeight() )
+    self.offset = Vector2( 0, 0 )
+end
+
+----- INSTANCE METHODS -----
+
+
+----- IMPLEMENTED METHODS -----
+
+function Sprite:frameUpdate( deltaTime )
+
+    local position = self:getPosition()
+    local offset = Vector2(
+        self.centered and self.texture:getWidth() / 2 + self.offset.x or self.offset.x,
+        self.centered and self.texture:getHeight() / 2 + self.offset.y or self.offset.y
+    )
+
+    local flipFactor = Vector2( self.flipH and -1 or 1, self.flipV and -1 or 1 )
+
+    love.graphics.draw(
+        self.texture,
+        position.x,
+        position.y,
+        nil,
+        self.size.x / self.texture:getWidth() * flipFactor.x,
+        self.size.y / self.texture:getHeight() * flipFactor.y,
+        offset.x,
+        offset.y
+    )
+    love.graphics.setPointSize( 4 )
+    love.graphics.points( position.x, position.y )
+    love.graphics.setPointSize( 1 )
+end
+
+return Sprite
--- units/upgrader.lua
+++ units/upgrader.lua
@@ -1,5 +1,7 @@
 local GameObject2D = require( "units.game_object_2d" )
 local Vector2 = require( "units.vector2" )
+local Sprite = require( "units.sprite" )
+
 local collision = require( "units.collision" )
 
 
@@ -12,16 +14,16 @@ local Upgrader = GameObject2D:subclass( "Upgrader" )
 
 function Upgrader:initialize( pos, tag, beamColor, upgradeCallback )
     Upgrader.super.initialize( self )
-    self.beamColor = beamColor or DEFAULT_BEAM_COLOR
 
-    self.size = Vector2( 128, 128 )
-    self.image = love.graphics.newImage( "res/upgrader.png" )
+    self.sprite = Sprite( "res/upgrader.png" )
+    self.sprite.size = Vector2( 256, 256 )
+    self:addChild( self.sprite )
 
-    self.beam = {}
-    self.beam.size = Vector2( 8, self.size.y )
-    self.beam.pos = Vector2( 0, 0 )
     self.tag = tag
-
+    self.beam = {}
+    self.beam.size = Vector2( 8, self.sprite.size.y )
+    self.beam.pos = Vector2( 0, 0 )
+    self.beam.color = beamColor or DEFAULT_BEAM_COLOR
     self.callback = upgradeCallback
 
     self:setPosition( pos )
@@ -35,11 +37,9 @@ function Upgrader:checkCollision( objA, objB )
 end
 
 function Upgrader:frameUpdate()
-    local pos = self:getPosition()
-    love.graphics.draw( self.image, pos.x, pos.y, nil, self.size.x / self.image:getWidth(), self.size.y / self.image:getHeight() )
 
     local prevColor = { love.graphics.getColor() }
-    love.graphics.setColor( self.beamColor )
+    love.graphics.setColor( self.beam.color )
     love.graphics.rectangle(
         "fill",
         self.beam.pos.x,
