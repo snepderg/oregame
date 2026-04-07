ab9ede3364fdcfdac14f554fd17d425a176177a0
CriticalFloof <zackbig2005@gmail.com>
2026-04-06 22:52:25 -0400

    Refactor Sprite Positioning

--- units/sprite.lua
+++ units/sprite.lua
@@ -32,21 +32,20 @@ function Sprite:draw()
 
     local position = self:getPosition()
     local offset = Vector2(
-        self.centered and self.texture:getWidth() / 2 + self.offset.x or self.offset.x,
-        self.centered and self.texture:getHeight() / 2 + self.offset.y or self.offset.y
+        self.centered and self.size.x / 2 + self.offset.x or self.offset.x,
+        self.centered and self.size.y / 2 + self.offset.y or self.offset.y
     )
 
     local flipFactor = Vector2( self.flipH and -1 or 1, self.flipV and -1 or 1 )
+    local scaleFactor = Vector2( self.size.x / self.texture:getWidth() * flipFactor.x, self.size.y / self.texture:getHeight() * flipFactor.y )
 
     love.graphics.draw(
         self.texture,
-        position.x,
-        position.y,
+        position.x - offset.x,
+        position.y - offset.y,
         nil,
-        self.size.x / self.texture:getWidth() * flipFactor.x,
-        self.size.y / self.texture:getHeight() * flipFactor.y,
-        offset.x,
-        offset.y
+        scaleFactor.x,
+        scaleFactor.y
     )
     love.graphics.setPointSize( 4 )
     love.graphics.points( position.x, position.y )
