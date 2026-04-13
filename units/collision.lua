1b95330c1939519d991c95401edb72af8e77648e
CriticalFloof <zackbig2005@gmail.com>
2026-04-13 09:59:55 -0400

    Refactor Collision Script
    Removed local variables in AABB check

--- units/collision.lua
+++ units/collision.lua
@@ -1,20 +1,12 @@
 local collision = {}
 
-function collision.checkAABB( objA, objB )
-    local aLeft = objA._position.x
-    local aRight = objA._position.x + objA.size.x
-    local aTop = objA._position.y
-    local aBottom = objA._position.y + objA.size.y
-
-    local bLeft = objB._position.x
-    local bRight = objB._position.x + objB.size.x
-    local bTop = objB._position.y
-    local bBottom = objB._position.y + objB.size.y
-
-    return aRight > bLeft
-        and aLeft < bRight
-        and aBottom > bTop
-        and aTop < bBottom
+function collision.checkAABB( rectA, rectB )
+    return (
+        rectB.position.x < rectA.position.x + rectA.size.x and
+        rectA.position.x < rectB.position.x + rectB.size.x and
+        rectB.position.y < rectA.position.y + rectA.size.y and
+        rectA.position.y < rectB.position.y + rectB.size.y
+    )
 end
 
 return collision
